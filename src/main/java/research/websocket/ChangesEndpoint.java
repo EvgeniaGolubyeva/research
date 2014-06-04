6e3e2da3fe6123b90f3d7242114331f77e8e8de3
Evgenia Golubyeva <evgenia.golubyeva@gmail.com>
2014-06-03 21:57:40 -0700

    comment

--- src/main/java/research/websocket/ChangesEndpoint.java
+++ src/main/java/research/websocket/ChangesEndpoint.java
@@ -1,5 +1,7 @@
 package research.websocket;
 
+import research.entity.UserData;
+
 import javax.inject.Inject;
 import javax.websocket.OnClose;
 import javax.websocket.OnOpen;
@@ -13,9 +15,12 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint(value = "/server/changes")
 public class ChangesEndpoint {
     private WebSocketSessionHolder holder;
+    private UserData userData;
 
     @OnOpen
     public void openConnection(Session mySession) {
+        //cannot connect session with userId at this point
+        //http://stackoverflow.com/questions/21049387/using-a-cdi-sessionscoped-bean-from-a-websocket-serverendpoint
         holder.add(mySession);
     }
 
@@ -27,5 +32,10 @@ public class ChangesEndpoint {
     @Inject
     public void setHolder(WebSocketSessionHolder holder) {
         this.holder = holder;
+    }
+
+    @Inject
+    public void setUserData(UserData userData) {
+        this.userData = userData;
     }
 }
